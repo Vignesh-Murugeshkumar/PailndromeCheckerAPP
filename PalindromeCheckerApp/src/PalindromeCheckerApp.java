8c992b9cd9ccb2f5af15fb4388daeb23efa54574
Vignesh-Murugeshkumar <vigneshmurugeshkumar@gmail.com>
2026-02-23 15:23:22 +0530

    Palindrome Check Using String Reverse

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,5 +1,17 @@
 /**
+ * ============================================================
+ * MAIN CLASS -Palindrome Check Using String Reverse
+ * ===========================================================
+ * use case 3- Reverse string based palindrome
  *
+ * Description:
+ * this class checks eheter a string is palindrome by reverse string based
+ * the reverse dstring is checked with the original one
+ * At this stage , the application
+ *the application usesa a imple backward string matching and checks with the original string and validates it
+ *
+ * @author Vicky
+ * @version 3.0
  */
 public class PalindromeCheckerApp {
     public static void main(String[] args){
